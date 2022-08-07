e2d0703ec3b63bae4ad739fd83ef5bc472393fc0
matthew <marod424@gmail.com>
2022-08-07 15:13:21 -0400

    real time system with redis

--- server/src/socket/utils.py
+++ server/src/socket/utils.py
@@ -1,8 +1,20 @@
 from typing import Optional
 from fastapi import Query, WebSocket, status
+from ..redis.config import Redis
+
+redis = Redis()
 
 async def get_token(websocket: WebSocket, token: Optional[str] = Query(None)):
   if token is None or token == "":
     await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
   
-  return token
+  redis_client = await redis.create_connection()
+  exists = await redis_client.exists(token)
+
+  if exists == 1:
+    return token
+  else:
+    await websocket.close(
+      code=status.WS_1008_POLICY_VIOLATION, 
+      reason="Session not authenticated or expired token"
+    )
